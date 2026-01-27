484afee414dc056cab386d90b84daaadaf2f0a3c
James Forrest <jsforrest87@gmail.com>
2026-01-26 21:53:13 -0600

    Create core Data model skeleton

---
+++ source/Data/Models/AppIdea.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models
+{
+    internal class AppIdea
+    {
+    }
+}
---
+++ source/Data/Models/Palette.cs
@@ -0,0 +1,11 @@
+﻿using Data.ValueObjects;
+
+namespace Data.Models
+{
+    internal class Palette
+    {
+        public HexColor PrimaryColor { get; set; }
+        public HexColor SecondaryColor { get; set; }
+        public HexColor AccentColor { get; set; }
+    }
+}
