dbb971bd397bd6fdc5f95faebf88690621c6e4c8
James Forrest <jsforrest87@gmail.com>
2026-01-26 21:53:46 -0600

    Create HexColor value object implementing IEquatable

---
+++ source/Data/ValueObjects/HexColor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Data.ValueObjects
+{
+    public readonly struct HexColor : IEquatable<HexColor>
+    {
+        private static readonly Regex HexRegex = new("^#?[0-9A-Fa-f]{6}$", RegexOptions.Compiled);
+
+        public string Value { get; }
+
+        private HexColor(string value) => Value = value;
+
+        public static HexColor Parse(string input)
+        {
+            if (!TryParse(input, out var color))
+                throw new ArgumentException($"Invalid hex color: {input}");
+            return color;
+        }
+
+        private static bool TryParse(string? input, out HexColor color)
+        {
+            color = default;
+
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            if(!HexRegex.IsMatch(input))
+                return false;
+
+            color = new HexColor(Normalize(input));
+            return true;
+        }
+
+        private static string Normalize(string input)
+        {
+            var hex = input.StartsWith("#") ? input[1..] : input;
+            return $"#{hex.ToUpperInvariant()}";
+        }
+
+        public override string ToString() => Value;
+
+        public bool Equals(HexColor other) => Value == other.Value;
+
+        public override bool Equals(object? obj) => obj is HexColor other && Equals(other);
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public static bool operator ==(HexColor left, HexColor right) => left.Equals(right);
+        public static bool operator !=(HexColor left, HexColor right) => !left.Equals(right);
+
+    }
+}
