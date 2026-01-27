8723eaa44a305a643a2e6339c400422c189a7eb2
James Forrest <jsforrest87@gmail.com>
2026-01-26 22:08:25 -0600

    Create initial unit test for HexColor

---
+++ test/Data.UnitTests/Test_HexColor.cs
@@ -0,0 +1,21 @@
+﻿using Data.ValueObjects;
+using FluentAssertions;
+
+namespace Data.UnitTests
+{
+    public class Test_HexColor
+    {
+        [Theory]
+        [InlineData("#FFFFFF", "#FFFFFF")]
+        [InlineData("#000000", "#000000")]
+        [InlineData("#ff5733", "#FF5733")]
+        [InlineData("FF5733", "#FF5733")]
+        public void Parse_ValidHex_CreatesValueObject(string input, string expected)
+        {
+            var color = HexColor.Parse(input);
+            color.Value.Should().Be(expected);
+        }
+
+
+    }
+}
