79738e89cc9a483eb268757154e285413fe492df
James Forrest <jsforrest87@gmail.com>
2026-01-27 18:30:06 -0600

    Installed Microsoft EntityFrameworkCore Added EF value converter for HexColor

---
+++ source/Data/Converters/HexColorValueConverter.cs
@@ -0,0 +1,15 @@
+﻿using Data.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Data.Converters
+{
+    public sealed class HexColorValueConverter : ValueConverter<HexColor, string>
+    {
+        public HexColorValueConverter() 
+            : base(
+                  color => color.Value,
+                  value => HexColor.Parse(value))
+        {            
+        }
+    }
+}
