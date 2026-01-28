756b8490753740f3e9df3f1ac101a4b8406d862e
James Forrest <jsforrest87@gmail.com>
2026-01-27 18:24:16 -0600

    Removed unnecessary usings from HexColor Created Application project Created JsonConverter for HexColor

---
+++ source/Application/Converters/HexColorJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using Data.ValueObjects;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Application.Converters
+{
+    public sealed class HexColorJsonConverter : JsonConverter<HexColor>
+    {
+        public override HexColor Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return HexColor.Parse(value!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, HexColor value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.Value);
+        }
+    }
+}
