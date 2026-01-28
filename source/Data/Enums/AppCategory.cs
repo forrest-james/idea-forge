232126e339b4a31b66aecbcec60a7cce792d1804
James Forrest <jsforrest87@gmail.com>
2026-01-27 18:41:37 -0600

    Add AppType and AppCategory Enums

---
+++ source/Data/Enums/AppCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.Enums
+{
+    internal enum AppCategory
+    {
+        Productivity,
+        Entertainment,
+        Education,
+        Health,
+        Finance,
+        Social,
+        Utilities,
+        Games,
+        Lifestyle,
+        Travel
+    }
+}
---
+++ source/Data/Enums/AppType.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Enums
+{
+    internal enum AppType
+    {
+        Mobile,
+        Web,
+        Desktop,
+        ProgressiveWebApp
+    }
+}
