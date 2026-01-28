81e9b587d7434e011e992cb7f7c845ea06aff280
James Forrest <jsforrest87@gmail.com>
2026-01-27 18:56:37 -0600

    Add ImageUrl Value object and Image model

---
+++ source/Data/Models/Image.cs
@@ -0,0 +1,25 @@
+﻿using Data.ValueObjects;
+
+namespace Data.Models
+{
+    public class Image
+    {
+        public Guid Id { get; private set; }
+        public ImageUrl Url { get; private set; }
+        public int Order { get; private set; }
+
+        private Image() { }
+
+        internal Image(ImageUrl url, int order)
+        {
+            Id = Guid.NewGuid();
+            Url = url;
+            Order = order;
+        }
+
+        internal void SetOrder(int order)
+        {
+            Order = order;
+        }
+    }
+}
---
+++ source/Data/ValueObjects/ImageUrl.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.ValueObjects
+{
+    public sealed record ImageUrl
+    {
+        public string Value { get; }
+
+        private ImageUrl(string value)
+        {
+            Value = value;
+        }
+
+        public static ImageUrl Create(string value)
+        {
+            if (!Uri.TryCreate(value, UriKind.Absolute, out var uri))
+                throw new CannotUnloadAppDomainException("Invalid image URL");
+
+            return new ImageUrl(uri.ToString());
+        }
+
+        public override string ToString() => Value;
+    }
+}
