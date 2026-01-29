e688bc03704bcac2f5b4314e2bdae1e1f33737fa
James Forrest <jsforrest87@gmail.com>
2026-01-28 20:40:58 -0600

    Fixed missing semi-colon in AppDbContext

--- source/Data/EF/AppDbContext.cs
+++ source/Data/EF/AppDbContext.cs
@@ -62,7 +62,7 @@ namespace Data.EF
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=IdeaForge_Local;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30")
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=IdeaForge_Local;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
         }
     }
 }
