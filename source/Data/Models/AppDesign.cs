a09d482001117d7f95cbfd8ab361c72f34390057
James Forrest <jsforrest87@gmail.com>
2026-01-28 20:11:18 -0600

    Add properties and domain behavior to AppDesign model

--- source/Data/Models/AppDesign.cs
+++ source/Data/Models/AppDesign.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Data.Exceptions;
+using Data.ValueObjects;
 
 namespace Data.Models
 {
-    internal class AppDesign
+    public class AppDesign
     {
+        private readonly List<Image> _images = new();
+
+        public Guid Id { get; private set; }
+        public Challenge Challenge { get; private set; }
+        public string Description { get; private set; }
+        public IReadOnlyCollection<Image> Images => _images.OrderBy(i => i.Order).ToList();
+
+        private AppDesign() { }
+
+        public AppDesign(Challenge challenge, string description)
+        {
+            Id = Guid.NewGuid();
+            Challenge = challenge;
+            Description = description;
+        }
+
+        public void AddImage(ImageUrl url)
+        {
+            var order = _images.Count;
+            _images.Add(new Image(url, order));
+        }
+
+        public void RemoveImage(Guid imageId)
+        {
+            var image = _images.SingleOrDefault(i => i.Id == imageId);
+            if (image is null)
+                throw new DomainException("Image not found");
+
+            _images.Remove(image);
+            ReorderImages();
+        }
+
+        public void ReorderImages()
+        {
+            for (int i = 0; i < _images.Count; i++)
+            {
+                _images[i].SetOrder(i);
+            }
+        }
     }
 }
