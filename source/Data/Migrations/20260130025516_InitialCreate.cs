514565e676a2c96a8594201972f4068be9b2fb32
James Forrest <jsforrest87@gmail.com>
2026-01-29 20:56:10 -0600

    Initial EF Migration

---
+++ source/Data/Migrations/20260130025516_InitialCreate.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppIdeas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppIdeas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Palettes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PrimaryColor = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    SecondaryColor = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    AccentColor = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Palettes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Challenges",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppIdeaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PaletteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Challenges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Challenges_AppIdeas_AppIdeaId",
+                        column: x => x.AppIdeaId,
+                        principalTable: "AppIdeas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Challenges_Palettes_PaletteId",
+                        column: x => x.PaletteId,
+                        principalTable: "Palettes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppDesigns",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ChallengeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PaletteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppDesigns", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppDesigns_Challenges_ChallengeId",
+                        column: x => x.ChallengeId,
+                        principalTable: "Challenges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppDesigns_Palettes_PaletteId",
+                        column: x => x.PaletteId,
+                        principalTable: "Palettes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AppDesignId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_AppDesigns_AppDesignId",
+                        column: x => x.AppDesignId,
+                        principalTable: "AppDesigns",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppDesigns_ChallengeId",
+                table: "AppDesigns",
+                column: "ChallengeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppDesigns_PaletteId",
+                table: "AppDesigns",
+                column: "PaletteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Challenges_AppIdeaId",
+                table: "Challenges",
+                column: "AppIdeaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Challenges_PaletteId",
+                table: "Challenges",
+                column: "PaletteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_AppDesignId",
+                table: "Images",
+                column: "AppDesignId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "AppDesigns");
+
+            migrationBuilder.DropTable(
+                name: "Challenges");
+
+            migrationBuilder.DropTable(
+                name: "AppIdeas");
+
+            migrationBuilder.DropTable(
+                name: "Palettes");
+        }
+    }
+}
