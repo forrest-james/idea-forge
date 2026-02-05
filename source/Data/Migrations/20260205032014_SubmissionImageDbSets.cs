5b3c7245ac7a9cffe7a975881806a80d3ff3b6f1
James Forrest <jsforrest87@gmail.com>
2026-02-04 21:21:13 -0600

    Add migration for AppDbContext update

---
+++ source/Data/Migrations/20260205032014_SubmissionImageDbSets.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubmissionImageDbSets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Image_Submission_SubmissionId",
+                table: "Image");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Submission_Challenges_ChallengeId",
+                table: "Submission");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Submission",
+                table: "Submission");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Image",
+                table: "Image");
+
+            migrationBuilder.RenameTable(
+                name: "Submission",
+                newName: "Submissions");
+
+            migrationBuilder.RenameTable(
+                name: "Image",
+                newName: "Images");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Submission_ChallengeId",
+                table: "Submissions",
+                newName: "IX_Submissions_ChallengeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Image_SubmissionId",
+                table: "Images",
+                newName: "IX_Images_SubmissionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Submissions",
+                table: "Submissions",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Images",
+                table: "Images",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Submissions_SubmissionId",
+                table: "Images",
+                column: "SubmissionId",
+                principalTable: "Submissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Submissions_Challenges_ChallengeId",
+                table: "Submissions",
+                column: "ChallengeId",
+                principalTable: "Challenges",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Submissions_SubmissionId",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Submissions_Challenges_ChallengeId",
+                table: "Submissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Submissions",
+                table: "Submissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Images",
+                table: "Images");
+
+            migrationBuilder.RenameTable(
+                name: "Submissions",
+                newName: "Submission");
+
+            migrationBuilder.RenameTable(
+                name: "Images",
+                newName: "Image");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Submissions_ChallengeId",
+                table: "Submission",
+                newName: "IX_Submission_ChallengeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Images_SubmissionId",
+                table: "Image",
+                newName: "IX_Image_SubmissionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Submission",
+                table: "Submission",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Image",
+                table: "Image",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Image_Submission_SubmissionId",
+                table: "Image",
+                column: "SubmissionId",
+                principalTable: "Submission",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Submission_Challenges_ChallengeId",
+                table: "Submission",
+                column: "ChallengeId",
+                principalTable: "Challenges",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
