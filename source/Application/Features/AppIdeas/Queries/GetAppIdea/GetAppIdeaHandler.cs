8ff03397224307d55c9e706870dc93a5b678d04b
James Forrest <jsforrest87@gmail.com>
2026-02-08 17:33:27 -0600

    Get AppIdea vertical slice

---
+++ source/Application/Features/AppIdeas/Queries/GetAppIdea/GetAppIdeaHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.AppIdeas.DTOs;
+using Data.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.AppIdeas.Queries.GetAppIdea
+{
+    public sealed class GetAppIdeaHandler : IRequestHandler<GetAppIdeaQuery, AppIdeaDto?>
+    {
+        private readonly IAppDbContext _dbContext;
+        public GetAppIdeaHandler(IAppDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<AppIdeaDto?> Handle(GetAppIdeaQuery request, CancellationToken cancellationToken)
+        {
+            var raw = await _dbContext.AppIdeas
+                .AsNoTracking()
+                .Where(a => a.Id == request.Id)
+                .Select(a => new { a.Id, a.Type, a.Category, a.Description })
+                .FirstOrDefaultAsync(cancellationToken);
+
+            return raw is null
+                ? null
+                : new AppIdeaDto
+                (
+                    raw.Id,
+                    raw.Type.ToString(),
+                    raw.Category.ToString(),
+                    raw.Description
+                );
+        }
+    }
+}
---
+++ source/Application/Features/AppIdeas/Queries/GetAppIdea/GetAppIdeaQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Features.AppIdeas.DTOs;
+using MediatR;
+
+namespace Application.Features.AppIdeas.Queries.GetAppIdea
+{
+    public sealed record GetAppIdeaQuery(Guid Id) : IRequest<AppIdeaDto?>;
+}
