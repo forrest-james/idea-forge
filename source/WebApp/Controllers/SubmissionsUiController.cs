a912a550983bcabb6e950180000c857a3c0cc627
James Forrest <jsforrest87@gmail.com>
2026-02-13 09:04:15 -0600

    Built Submissions display front end

---
+++ source/WebApp/Controllers/SubmissionsUiController.cs
@@ -0,0 +1,41 @@
+﻿using Application.Features.Submissions.Queries.GetSubmission;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels.Submissions;
+
+namespace WebApp.Controllers
+{
+    [Route("submissions")]
+    public class SubmissionsUiController : Controller
+    {
+        private readonly IMediator _mediator;
+        public SubmissionsUiController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> Details(Guid id, CancellationToken cancellationToken)
+        {
+            var dto = await _mediator.Send(new GetSubmissionQuery(id), cancellationToken);
+            if (dto is null) return NotFound();
+
+            var vm = new SubmissionDetailsVm
+            {
+                Id = dto.Id,
+                Description = dto.Description,
+                ChallengeId = dto.Challenge.Id,
+                ChallengeName = dto.Challenge.Name,
+                Images = dto.Images
+                    .OrderBy(i => i.Order)
+                    .Select(i => new SubmissionImageVm
+                    {
+                        Id = i.Id,
+                        ImageUrl = i.ImageUrl,
+                        Order = i.Order
+                    })
+                    .ToList(),
+                UploadDisabled = true
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ source/WebApp/ViewModels/Submissions/SubmissionDetailsVm.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApp.ViewModels.Submissions
+{
+    public sealed class SubmissionDetailsVm
+    {
+        public Guid Id { get; init; }
+        public string Description { get; init; } = "";
+        public Guid ChallengeId { get; init; }
+        public string ChallengeName { get; init; } = "";
+        public List<SubmissionImageVm> Images { get; init; } = new();
+        public bool UploadDisabled { get; init; } = true;
+    }
+
+    public sealed class SubmissionImageVm
+    {
+        public Guid Id { get; init; }
+        public string ImageUrl { get; init; } = "";
+        public int Order { get; init; }
+    }
+}
