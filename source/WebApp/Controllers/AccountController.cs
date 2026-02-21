ce17727bcc2fbd49856bae4c530d0882023c0d61
James Forrest <jsforrest87@gmail.com>
2026-02-21 07:00:29 -0600

    Add Login and Logout functionality

---
+++ source/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels.Account;
+
+namespace WebApp.Controllers
+{
+    [AllowAnonymous]
+    [Route("account")]
+    public sealed class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(SignInManager<IdentityUser> signInManager) => _signInManager = signInManager;
+
+        [HttpGet("login")]
+        public IActionResult Login(string? returnUrl = null) => View(new LoginVm { ReturnUrl = returnUrl ?? "/" });
+
+        [HttpPost("login")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVm vm, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName: vm.Email,
+                password: vm.Password,
+                isPersistent: vm.RememberMe,
+                lockoutOnFailure: false);
+
+            if(!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Invalid email or password.");
+                return View(vm);
+            }
+
+            if (!string.IsNullOrWhiteSpace(vm.ReturnUrl) && Url.IsLocalUrl(vm.ReturnUrl))
+                return Redirect(vm.ReturnUrl);
+
+            return Redirect("/");
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
---
+++ source/WebApp/ViewModels/Account/LoginVm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels.Account
+{
+    public sealed class LoginVm
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = "";
+
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; } = "";
+    }
+}
